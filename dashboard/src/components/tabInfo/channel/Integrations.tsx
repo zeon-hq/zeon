053d51925dbe8a9dde46fb01545fbcfd3a9eb701
Ajay <>
2023-12-16 03:09:37 +0530

    integrations css

--- dashboard/src/components/tabInfo/channel/Integrations.tsx
+++ dashboard/src/components/tabInfo/channel/Integrations.tsx
@@ -1,24 +1,24 @@
-import { Text, Button, Divider, Flex, Box, Space } from "@mantine/core"
-import { BrandSlack, Plus } from "tabler-icons-react"
-import useDashboard from "hooks/useDashboard"
-import Heading from "components/details/inbox/component/Heading"
+import { Text, Button, Divider, Flex, Box, Space } from "@mantine/core";
+import { BrandSlack, Plus } from "tabler-icons-react";
+import useDashboard from "hooks/useDashboard";
+import Heading from "components/details/inbox/component/Heading";
 
-type Props = {}
+type Props = {};
 
 const Integrations = (props: Props) => {
-  const { workspaceInfo } = useDashboard()
-  const { selectedPage, channelsInfo } = useDashboard()
+  const { workspaceInfo } = useDashboard();
+  const { selectedPage, channelsInfo } = useDashboard();
   const handleIntegrateSlack = () => {
-    if (channelsInfo[selectedPage.name]?.channelId) return
+    if (channelsInfo[selectedPage.name]?.channelId) return;
     localStorage.setItem(
       "userstak-dashboard-workspaceId",
       workspaceInfo.workspaceId || ""
-    )
+    );
     window.open(
       "https://slack.com/oauth/v2/authorize?client_id=3769512251410.4269280938033&scope=channels:history,channels:join,channels:read,chat:write,incoming-webhook,users.profile:read&user_scope=",
       "_blank"
-    )
-  }
+    );
+  };
 
   return (
     <div>
@@ -26,45 +26,75 @@ const Integrations = (props: Props) => {
         heading="Integrations"
         subheading="Supercharge your workflow and connect the tools you use every day."
       />
+      <Space h={20} />
+      {
+        <Flex gap="32px" align="center" justify="space-between">
+          <Flex gap="16px">
+            <Flex align="center">
+              <BrandSlack size={30} />
+            </Flex>
 
-      <Flex align="center" justify="space-between">
-        <Flex gap="10px">
-          <Flex align="center">
-            <BrandSlack size={30} />
+            <Box>
+              <Text weight="600" size="14px">
+                {" "}
+                Slack{" "}
+              </Text>
+              <Text color="#475467" weight="400" size="14px">
+                {" "}
+                Send incoming conversations and respond to them within slack as
+                threads{" "}
+              </Text>
+            </Box>
           </Flex>
+          <Flex gap="16px">
+            <Button
+              radius="sm"
+              onClick={handleIntegrateSlack}
+              disabled={!!channelsInfo[selectedPage.name]?.channelId}
+              leftIcon={<Plus />}
+              sx={{ backgroundColor: "#F5F8FF", color: "#3054B9" }}
+            >
+              {" "}
+              {channelsInfo[selectedPage.name]?.channelId
+                ? "Disconnect"
+                : "Connect"}{" "}              
+            </Button>
+          </Flex>
+          <Flex gap="16px">
+            <Flex align="center">
+              <BrandSlack size={30} />
+            </Flex>
 
-          <Box>
-            <Text weight="600" size="lg">
+            <Box>
+              <Text weight="600" size="14px">
+                {" "}
+                Slack{" "}
+              </Text>
+              <Text color="#475467" weight="400" size="14px">
+                {" "}
+                Send incoming conversations and respond to them within slack as
+                threads{" "}
+              </Text>
+            </Box>
+          </Flex>
+          <Flex gap="16px">
+            <Button
+              radius="sm"
+              onClick={handleIntegrateSlack}
+              disabled={!!channelsInfo[selectedPage.name]?.channelId}
+              leftIcon={<Plus />}
+              sx={{ backgroundColor: "#F5F8FF", color: "#3054B9" }}
+            >
               {" "}
-              Slack{" "}
-            </Text>
-            <Text color="#475467">
-              {" "}
-              Send incoming conversations and respond to them within slack as
-              threads{" "}
-            </Text>
-          </Box>
+              {channelsInfo[selectedPage.name]?.channelId
+                ? "Disconnect"
+                : "Connect"}{" "}
+            </Button>
+          </Flex>
         </Flex>
-        <Flex gap="10px">
-          <Button radius="md" color="dark" variant="outline">
-            Read Docs
-          </Button>
-          <Button
-            radius="md"
-            onClick={handleIntegrateSlack}
-            disabled={!!channelsInfo[selectedPage.name]?.channelId}
-            leftIcon={<Plus />}
-            sx={{ backgroundColor: "#F5F8FF", color: "#3054B9" }}
-          >
-            {" "}
-            {channelsInfo[selectedPage.name]?.channelId
-              ? "Slack channel added"
-              : "Integrate to slack "}{" "}
-          </Button>
-        </Flex>
-      </Flex>
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Integrations
+export default Integrations;
