2915f80a97f71854e24174ca2e1e16eef2e27292
kaushalendra-pandey <pandeysid713@gmail.com>
2024-01-01 23:44:57 +0530

    wip

--- dashboard/src/crm/Contacts/CreateContact.tsx
+++ dashboard/src/crm/Contacts/CreateContact.tsx
@@ -41,20 +41,20 @@ function CreateContact() {
       phoneNumber: editValues?.phoneNumber || "",
       linkedInUrl: editValues?.linkedInUrl || "",
     },
-    validate: {
-      emailAddress: (value) =>
-        /^\S+@\S+$/.test(value) ? null : "Invalid email",
-      phoneNumber: (value) =>
-        /^\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/.test(
-          value
-        )
-          ? null
-          : "Invalid phone number, Please use international format",
-      linkedInUrl: (value) =>
-        /^https:\/\/www.linkedin.com\/.*/.test(value)
-          ? null
-          : "Invalid LinkedIn URL",
-    },
+    // validate: {
+    //   emailAddress: (value) =>
+    //     /^\S+@\S+$/.test(value) ? null : "Invalid email",
+    //   phoneNumber: (value) =>
+    //     /^\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/.test(
+    //       value
+    //     )
+    //       ? null
+    //       : "Invalid phone number, Please use international format",
+    //   linkedInUrl: (value) =>
+    //     /^https:\/\/www.linkedin.com\/.*/.test(value)
+    //       ? null
+    //       : "Invalid LinkedIn URL",
+    // },
   })
 
   const labelStyles = {
