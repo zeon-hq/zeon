4b7ec5f174aebc902ae8783a4becff643f52e68a
Kaushalendra Pandey <67643722+kaushalendra-pandey@users.noreply.github.com>
2024-01-13 00:04:43 +0530

    prettier change

--- zeon-finance/index.ts
+++ zeon-finance/index.ts
@@ -1,42 +1,46 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import { verifyIdentity, initializeDB } from 'zeon-core/dist/func'
+import { verifyIdentity, initializeDB } from "zeon-core/dist/func";
 import cors from "cors";
 import expenseRoutes from "./routes/expense";
 import categoryRoutes from "./routes/category";
 import rootRoutes from "./routes/root";
-import mongoose from "mongoose"
-import  { Request, Response } from "express";
-
-
+import mongoose from "mongoose";
+import { Request, Response } from "express";
 
 mongoose
   //@ts-ignore
-  .connect(process.env.DB_URI, {dbName: process.env.DB_NAME})
+  .connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
   .then(() => {
     console.log("Connected to DB from finance");
-    initializeDB()
+    initializeDB();
   })
   .catch((e) => {
     console.log(e);
   });
 
 const app = express();
-const port = process.env.FINANCE_BACKEND_PORT
-
+const port = process.env.FINANCE_BACKEND_PORT;
 
 // setup cors to allow all origins
-app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001","https://dev.zeonhq.com", "https:app.zeonhq.com"],
-  // ALLOW ALL METHODs
-  methods: "*",
-  // ALLOW ALL HEADERS
-  allowedHeaders: "*",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://dev.zeonhq.com",
+      "https:app.zeonhq.com",
+    ],
+    // ALLOW ALL METHODs
+    methods: "*",
+    // ALLOW ALL HEADERS
+    allowedHeaders: "*",
+    credentials: true,
+  })
+);
 
-  // set up router
+// set up router
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -44,15 +48,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/expense", expenseRoutes);
 app.use("/category", categoryRoutes);
 app.use("/", rootRoutes);
-app.get("/health", (req: Request, res: Response)=>{
-  console.log('finance service health check');
+app.get("/health", (req: Request, res: Response) => {
+  console.log("finance service health check");
   res.send("all ok from zeon finance service");
 });
-
-
 
 // run server at port 6000
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
-
