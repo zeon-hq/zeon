436395eccae44e6c338eaf1ec5e39c958648ed3d
Ajay <>
2024-01-17 15:26:03 +0530

    copilotconfig

--- dashboard/src/components/topbar/Widget.tsx
+++ dashboard/src/components/topbar/Widget.tsx
@@ -24,17 +24,17 @@ const Widget = () => {
   const userEmail = user.email;
 
   const options = {
-    apiUrl: Config('API_URL'), // your base url where your are hosting OpenCopilot at (the API), usually it's http://localhost:5000/api
-    socketUrl: Config('SOCKET_URL'), // the url of the socket url that will be used to send and receive messages
-    initialMessage: "How are the things", // optional: you can pass an array of messages that will be sent to the copilot when it's initialized
-    token: "GiTH4rF0i4ETgpDh", // you can get your token from the dashboard
+    apiUrl: Config("API_URL"), // your base url where your are hosting OpenCopilot at (the API), usually it's http://localhost:5000/api
+    socketUrl: Config("SOCKET_URL"), // the url of the socket url that will be used to send and receive messages
+    initialMessage: "Hey there! I'm Robyn, your AI co-pilot. I can walk you through Zeon or help you carry out tasks for you. Ask me anything.", // optional: you can pass an array of messages that will be sent to the copilot when it's initialized
+    token: "9TdVM5y4QQ1vmMiq", // you can get your token from the dashboard
     defaultOpen: true,
     user: {
-      name: `${userFullName}`
+      name: `${userFullName}`,
     },
     headers: {
       // optional: you can pass your authentication tokens to the copilot or any other header you want to send with every request
-      Authorization: `Bearer ${getAuthToken()}`
+      Authorization: `Bearer ${getAuthToken()}`,
     },
   };
 
