03dfb645e707072e432e21a5982f1698c12c5eef
sathithyayogi <sathithyayogi99@gmail.com>
2024-01-21 17:41:49 +0530

    replying to thread from chat widget

--- ticket-backend/services/CoreService.ts
+++ ticket-backend/services/CoreService.ts
@@ -1,8 +1,12 @@
-import axios from "axios";
-import { AxiosResponse } from "axios";
-import { ISocketTicketPayload } from "../app";
-import { ITicketOptions } from "../schema/types/ticket";
-import { ILocationApiResponse } from "../type/types";
+import axios, { AxiosResponse } from "axios";
+
+export interface ISendSlackMessage {
+    channelId: string;
+    message?: string;
+    token: string;
+    blocks?: any;
+    thread_ts?: string;
+}
 
 export default class CoreService {
     public static sendMail = async (ticketMessage:string, toEmail:string, fromEmail:string, ticketId:string, channelId:string, workspaceId:string): Promise<any> => {
@@ -39,72 +43,10 @@ export default class CoreService {
         });
     }
 
-    public static sendSlackMessage = async (channel:any, ticketOptions:ITicketOptions, ticketPayload:ISocketTicketPayload, ticketId:string, locationData:ILocationApiResponse): Promise<any> => {
+    public static sendSlackMessage = async (sendSlackPayload:ISendSlackMessage): Promise<any> => {
         return new Promise((resolve, reject) => {
-            let locationName;
-            if (!locationData?.city || !locationData?.regionName || !locationData?.country) {
-                locationName = 'Not Found'
-            } else {
-                locationName = `${locationData.city}, ${locationData.regionName}, ${locationData.country}` || '';
-            }
+            const slackURL = process.env.CORE_SERVICE_URL + '/internal/slack/message';
             try {
-                const slackURL = process.env.CORE_SERVICE_URL + '/internal/slack/message';
-                console.log('slackURLHost', slackURL);
-                const blocks=[
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": `You have a new ticket:\n${ticketOptions.message}`
-                        }
-                    },
-                    {
-                        "type": "divider"
-                    },
-                    {
-                        "type": "section",
-                        "fields": [
-                            {
-                                "type": "mrkdwn",
-                                "text": `Type:\n${channel.name}`
-                            },
-                            {
-                                "type": "mrkdwn",
-                                "text": `E-Mail:\n${ticketPayload.customerEmail}`
-                            },
-                            {
-                                "type": "mrkdwn",
-                                "text": `Ticket ID:\n${ticketPayload.ticketId}`
-                            },
-                            {
-                                "type": "mrkdwn",
-                                "text": `Location:\n${locationName}`
-                            }
-                        ]
-                    },
-                    {
-                        "type": "divider"
-                    },
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "Head over to your dashboard to reply"
-                        },
-                        "accessory": {
-                            "type": "button",
-                            "text": {
-                              "type": "plain_text",
-                              "text": "Go to chat ->"
-                            },
-                            "url": `${process.env.WEBSITE_URL}/${ticketOptions.workspaceId}/chat?channelId=${ticketOptions.channelId}&ticketId=${ticketId}`
-                          }
-                    }
-                ]
-                const sendSlackPayload = {
-                    channelId:channel.slackChannelId, message:ticketOptions.message, token:channel.accessToken, blocks
-                  }
-
                 return axios.post(slackURL, sendSlackPayload)
                     .then(async (response: AxiosResponse<any>) => {
                         if (response.status === 200) {
