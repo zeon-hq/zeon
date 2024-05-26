7960426d4e8049baacdf6e4d75e603f779b56f62
sathithyayogi <sathithyayogi99@gmail.com>
2024-05-26 17:28:08 +0530

    ai changes

--- core-module/constant/AIConstant.ts
+++ core-module/constant/AIConstant.ts
@@ -1,3 +1,3 @@
-export const chromaDbUrl = 'https://chroma.zeonhq.com';
+export const chromaDbUrl = 'http://192.168.1.10:8760';
 export const modelName = 'gpt-3.5-turbo';
 export const temperature = 0;
